3f350ca18302e081e96c8c40a9a154337892dd63
choudharyprince890 <choudharyprince890@gmail.com>
2023-03-15 17:45:23 +0530

    front end of the project is created

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask,render_template
+import pandas as pd
 
 app = Flask(__name__)
+df = pd.read_csv("cleaned_car_data.csv")
+# print(df.columns)
+
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    companies = df['company'].unique()
+    names = df['name'].unique()
+    years = df['year'].unique()
+    fuel_type = df['fuel_type'].unique()
+
+    return render_template('index.html', companies=companies,names=names,years=years,fuel_type=fuel_type)
 
 
 if __name__=="__main__":
